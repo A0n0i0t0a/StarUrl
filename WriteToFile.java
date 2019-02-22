fe51e175704d826d35a62312bd78fda03424a82b
Anita Kumari <36147993+A0n0i0t0a@users.noreply.github.com>
2019-02-22 15:42:54 +0530

    Add files via upload

---
+++ WriteToFile.java
@@ -0,0 +1,29 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class WriteToFile 
+{
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		try
+		{
+			FileWriter writer=new FileWriter("D:\\names.txt",true);
+			BufferedWriter bwr=new BufferedWriter(writer);
+			bwr.write("James");
+			//bwr.write("\n");
+			bwr.newLine();
+			bwr.write("Hobert");
+			bwr.close();
+			System.out.println("succesfully written to a file");
+		}
+		catch(IOException ioe)
+		{
+			ioe.printStackTrace();
+		}
+
+	}
+
+}
